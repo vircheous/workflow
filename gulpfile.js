4f5aa9b0a276d404b70e74886f85ea34622a2730
Sinna <Sinna@Sinnas-MacBook-Pro.local>
2014-11-03 09:28:41 -0500

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
+
+// CoffeeScript to JavaScript task
 
 var coffeeSources = ['components/coffee/tagline.coffee'],
-    coffeeDestinations = ['components/sass'];
+    coffeeDestination = 'components/scripts';
 
 gulp.task('coffee', function(){
     gulp.src(coffeeSources[0])
         .pipe(coffee({ bare: true })
             .on('error', gutil.log))
-        .pipe(gulp.dest(coffeeDestinations[0])
+        .pipe(gulp.dest(coffeeDestination)
+    )
+});
+
+
+// Concatenate all JavaScript files into one.
+
+var jsSources = [
+        'components/scripts/rclick.js',
+        'components/scripts/pixgrid.js',
+        'components/scripts/tagline.js',
+        'components/scripts/template.js'
+    ],
+
+    jsDestination ='builds/development/js';
+
+gulp.task('js', function () {
+    // 1. Specify source
+    // 2. Pipe it through a a method.
+    // 3. Send it to a destination
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest(jsDestination)
     )
 });
